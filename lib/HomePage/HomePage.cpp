dde84ecc3e42b777d9dde3138d7e679658d4ea63
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:21:58 +0500

    Update HomePage.cpp

--- lib/HomePage/HomePage.cpp
+++ lib/HomePage/HomePage.cpp
@@ -1,83 +1,70 @@
 // HomePage.cpp
 #include "HomePage.h"
-#include <Adafruit_ST7735.h>
+#include <Adafruit_ILI9341.h>
 #include <Adafruit_SSD1306.h>
+#include <ColorPalette.h>
 
-int currentIndex = 0;
-void HomePage::onButtonPressed() {
+void HomePage::onShortClick() {
     currentIndex++;
     if (currentIndex >= 5) currentIndex = 0;
     previousIndex = -1;
 }
 
+void HomePage::onLongClick() {
+    pageManager->SwitchToIndex(currentIndex);
+    OnExit();
+}
+
 void HomePage::DrawGUI() {
-    _tft->fillScreen(ST7735_BLUE);
+    _tft->fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_BLUE);
     _tft->setCursor (2, 5);
     _tft->setTextSize(1);
-    _tft->setTextColor(ST7735_WHITE);
+    _tft->setTextColor(COLOR_WHITE);
     _tft->print("HOME");
-    _tft->drawLine(0, 13, SCREEN_WIDTH, 13, ST7735_WHITE);
-    _tft->drawLine(0, 117, 160, 117, ST7735_WHITE);
+    _tft->drawLine(0, 13, SCREEN_WIDTH, 13, COLOR_WHITE);
+    _tft->drawLine(0, 117, 160, 117, COLOR_WHITE);
     _tft->setCursor(2, 120);
     _tft->setTextSize(1);
-    _tft->setTextColor(ST7735_WHITE);
+    _tft->setTextColor(COLOR_WHITE);
     _tft->print("<== Prev");
     _tft->setCursor(110, 120);
     _tft->setTextSize(1);
-    _tft->setTextColor(ST7735_WHITE);
+    _tft->setTextColor(COLOR_WHITE);
     _tft->print("Next ==>");
 }
 
-void HomePage::Weather() {
-    _tft->setCursor(2, 18);
+void HomePage::DrawMenu() {
+    for (int i = 0; i < 5; i++) {
+    int y = charStart + charOffset * i;
+    
+    if (i == currentIndex) {
+        _tft->fillRect(0, y - 2, SCREEN_WIDTH, charOffset, COLOR_WHITE);
+        _tft->setTextColor(COLOR_BLUE);
+    } else {
+        _tft->fillRect(0, y - 2, SCREEN_WIDTH, charOffset, COLOR_BLUE);
+        _tft->setTextColor(COLOR_WHITE);
+        }
+        
+    _tft->setCursor(2, y);
     _tft->setTextSize(globalTextSize);
-    _tft->setTextColor(ST7735_WHITE);
-    _tft->print(menuItems[0]);
-}
-
-void HomePage::Tasks() {
-    _tft-> setCursor(2, 28);
-    _tft-> setTextSize(globalTextSize);
-    _tft-> setTextColor(ST7735_WHITE);
-    _tft-> print(menuItems[1]);
-}
-
-void HomePage::Time_Zones() {
-    _tft-> setCursor (2, 38);
-    _tft-> setTextSize(globalTextSize);
-    _tft-> setTextColor(ST7735_WHITE);
-    _tft-> print(menuItems[2]);
-}
-
-void HomePage::Gambling() {
-    _tft-> setCursor (2, 48);
-    _tft-> setTextSize(globalTextSize);
-    _tft-> setTextColor(ST7735_WHITE);
-    _tft-> print(menuItems[3]);
-}
-
-void HomePage::Settings(){
-    _tft-> setCursor (2, 58);
-    _tft-> setTextSize(globalTextSize);
-    _tft-> setTextColor(ST7735_WHITE);
-    _tft-> print(menuItems[4]);    
+    _tft->print(menuItems[i]);
+    }
 }
 
 void HomePage::OnEnter() {
     DrawGUI();
-    Weather();
-    Tasks();
-    Time_Zones();
-    Gambling();
-    Settings();
+    DrawMenu();
 }
 
 void HomePage::Update(uint32_t deltaTimeMs) {
-
+    if (currentIndex != previousIndex) {
+    DrawMenu();
+    previousIndex = currentIndex;
+    }
 }
 
 void HomePage::OnExit() {
-
+    _tft->fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_BLACK);
 }
 
 void HomePage::Draw() {
