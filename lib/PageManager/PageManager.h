7e432a831c338f646a3ba40b83790a2ba8e10796
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:24:22 +0500

    Update PageManager.h

--- lib/PageManager/PageManager.h
+++ lib/PageManager/PageManager.h
@@ -1,35 +1,15 @@
-#include "PageManager.h"
+#pragma once
+#include "Page.h"
+#include <vector>
 
-void PageManager::addPage(Page* page) {
-    _pages.push_back(page);
-}
-
-void PageManager::next() {
-    if(_pages.empty()) return;
-    _pages[_currentIndex]->OnExit();
-    _currentIndex = (_currentIndex + 1) % _pages.size();
-    _pages[_currentIndex]->OnEnter();
-}
-
-void PageManager::prev() {
-    if(_pages.empty()) return;
-    _pages[_currentIndex]->OnExit();
-    _currentIndex = (_currentIndex == 0)
-    ? _pages.size() - 1
-    : _currentIndex - 1;
-    _pages[_currentIndex]->OnEnter();
-}
-
-Page* PageManager::getCurrent() {
-    return _pages[_currentIndex];
-}
-
-void PageManager::SwitchToIndex(int index) {
-    if (_pages.empty()) return;
-    if (index < 0 || index >= (int)_pages.size()) return;
-    if (index == _currentIndex) return; 
-    
-    _pages[_currentIndex]->OnExit();
-    _currentIndex = index;
-    _pages[_currentIndex]->OnEnter();
-}
+class PageManager {
+    public:
+        void addPage(Page* page);
+        void next();
+        void prev();
+        void SwitchToIndex(int index);
+        Page* getCurrent();
+    private:
+        std::vector<Page*> _pages;
+        uint8_t _currentIndex = 0;
+};
