99b94a1f64edc650350a2d9c55e7f7ec362a7bcf
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:30:14 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@
 #define OLED_SCL  22   // I2C Clock
 #define OLED_ADDR 0x3C // Адрес OLED экрана
 #define OLED_RST  -1   // Пин Reset (если нет - ставим -1)
-#define BUTTON_PIN 13 
+#define BUTTON_PIN 4 
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
@@ -33,11 +33,11 @@ ScreenSaver screenSaver(&tft, &oled);
 
 void setup() {
     Serial.begin(115200);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
     tft.initR(INITR_BLACKTAB);
     tft.setRotation(1);
     tft.fillScreen(ILI9341_BLACK);
     oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
     button.setClickTicks(300u);
     button.setPressTicks(500u); 
     pm.addPage(&homePage);
