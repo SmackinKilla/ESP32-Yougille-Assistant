3de83f32552fab0d35fd807714a871e379146313
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:22:24 +0500

    Update HomePage.h

--- lib/HomePage/HomePage.h
+++ lib/HomePage/HomePage.h
@@ -1,31 +1,31 @@
 // HomePage.h
 #pragma once
 #include "Page.h"
+#include "PageManager.h"
 
 const int SCREEN_WIDTH = 160;
 const int SCREEN_HEIGHT = 128;
 
 class HomePage : public Page {
 public:
-    HomePage(Adafruit_ST7735* tft, Adafruit_SSD1306* oled) 
+    HomePage(Adafruit_ST7735* tft, Adafruit_SSD1306* oled, PageManager* pm) 
         : Page(tft, oled) {}
 
-    void onButtonPressed() override; 
+    void onShortClick() override;
+    void onLongClick() override;
     void OnEnter() override;
     void Update(uint32_t deltaTimeMs) override;
     void OnExit() override;
     void Draw() override;
 
 private:
+    PageManager* pageManager;
+    void DrawMenu();
     void DrawGUI();
-    void Weather();
-    void Tasks();
-    void Time_Zones();
-    void Gambling();
-    void Settings();
     int currentIndex = 0;
-    int previousIndex;
-    int charOffset;
+    int previousIndex = -1;
+    int charOffset = 10;
+    int charStart = 18;
     int globalTextSize = 1;
     const char* menuItems[5] {
         "1. Weather",
