6ba1fc86b6a3d973bb58287b0c2cc4fb809db1da
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:27:35 +0500

    Add files via upload

---
+++ ColorPalette.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Adafruit_ILI9341.h>
+
+constexpr uint16_t COLOR_BLUE        = ILI9341_BLUE;
+constexpr uint16_t COLOR_WHITE      = ILI9341_WHITE;
+constexpr uint16_t COLOR_BLACK = ILI9341_BLACK;
+constexpr uint16_t COLOR_RED       = ILI9341_RED;
+constexpr uint16_t COLOR_GREEN     = ILI9341_GREEN;
+constexpr uint16_t COLOR_CYAN     = ILI9341_CYAN;
+constexpr uint16_t COLOR_YELLOW    = ILI9341_YELLOW;
