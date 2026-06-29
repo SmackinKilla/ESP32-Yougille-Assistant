10b074ef23c8d18446e0542aaf9b136444fe8c83
SmackinKilla <122652943+SmackinKilla@users.noreply.github.com>
2026-06-29 14:28:39 +0500

    Update ColorPalette.h

--- ColorPalette.h
+++ ColorPalette.h
@@ -1,10 +1,10 @@
 #pragma once
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_ST7735.h>
 
-constexpr uint16_t COLOR_BLUE        = ILI9341_BLUE;
-constexpr uint16_t COLOR_WHITE      = ILI9341_WHITE;
+constexpr uint16_t COLOR_BLUE        = ST7735_BLUE;
+constexpr uint16_t COLOR_WHITE      = ST7735_WHITE;
 constexpr uint16_t COLOR_BLACK = ILI9341_BLACK;
-constexpr uint16_t COLOR_RED       = ILI9341_RED;
-constexpr uint16_t COLOR_GREEN     = ILI9341_GREEN;
-constexpr uint16_t COLOR_CYAN     = ILI9341_CYAN;
-constexpr uint16_t COLOR_YELLOW    = ILI9341_YELLOW;
+constexpr uint16_t COLOR_RED       = ST7735_RED;
+constexpr uint16_t COLOR_GREEN     = ST7735_GREEN;
+constexpr uint16_t COLOR_CYAN     = ST7735_CYAN;
+constexpr uint16_t COLOR_YELLOW    = ST7735_YELLOW;
